5d48b36d5d05ccbd34b9a093b6a5f2863ee63e31
l1xnan <l1xnan@qq.com>
2023-06-30 11:30:29 +0800

    fix: jupyter command output in stderr

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -55,29 +55,51 @@ pub fn create_server(state: State<ServerManagerState>, folder: &str) -> String {
   format!("http://localhost:{port}/lab?token={token}")
 }
 
-#[tauri::command]
-pub async fn get_running_servers() -> Vec<Server> {
-  let output = Command::new("jupyter")
-    .args(["lab", "list"])
-    .output()
-    .expect("failed to execute child");
+/// parse command output
+pub fn parse_output(bytes: Vec<u8>) -> Vec<Server> {
+  let content = String::from_utf8(bytes).unwrap();
+  println!("output: {}", content);
 
-  let stdout = String::from_utf8(output.stdout).unwrap();
+  let mut data: Vec<Server> = vec![];
 
-  let mut res: Vec<Server> = vec![];
-
-  for item in stdout.split("\n") {
-    if item.starts_with("http") {
+  for item in content.split("\n") {
+    if item.starts_with("http") || item.starts_with("[JupyterServerListApp]") {
       let servers: Vec<&str> = item.split("::").collect();
       if servers.len() == 2 {
-        let link = servers[0].trim();
+        let mut link = servers[0].trim();
+        if let Some(tmp) = link.strip_prefix("[JupyterServerListApp]") {
+          link = tmp.trim();
+        }
         let folder = servers[1].trim();
         println!("{:?}, {:?}", link, folder);
-        res.push(Server::new(link, folder))
+        data.push(Server::new(link, folder))
       }
     }
   }
-  res
+  data
+}
+
+#[tauri::command]
+pub async fn get_running_servers() -> Vec<Server> {
+  println!("query running server...");
+  let res = Command::new("jupyter").args(["lab", "list"]).output();
+
+  match res {
+    Ok(output) => {
+      let mut data: Vec<Server> = vec![];
+
+      let out_data = parse_output(output.stdout);
+      let err_data = parse_output(output.stderr);
+
+      data.extend(out_data);
+      data.extend(err_data);
+      data
+    }
+    Err(err) => {
+      println!("{:?}", err);
+      vec![]
+    }
+  }
 }
 
 /// fixed iframe security access issue:
@@ -99,7 +121,7 @@ c.ServerApp.tornado_settings["headers"]["Content-Security-Policy"] = "frame-ance
     let file = path.join(".jupyter/jupyter_lab_config.py");
     if file.exists() {
       let content = fs::read_to_string(file.clone()).unwrap();
-      
+
       if content.contains(conf) {
         println!("=== included `Content-Security-Policy` config.")
       } else {
